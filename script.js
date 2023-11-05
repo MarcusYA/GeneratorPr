75935a018855477c2430398b542b01f325654444
MarcusYA <marcus.78@hotmail.com>
2023-11-05 00:29:30 +0000

    slkfj

--- script.js
+++ script.js
@@ -6,19 +6,15 @@ const weather = () => {
     switch (nummer) {
         case 1:
             console.log("Snö")
-            ("#snow").html "Snö"
             break
         case 2:
             console.log("Sol")
-            ("#snow").html "Sol"
             break
         case 3:
             console.log("Regn")
-            ("#snow").html "Regn"
             break
         default:
             console.log("we do not know")
-            ("#snow").html "We do not know"
             break
     }
 }
